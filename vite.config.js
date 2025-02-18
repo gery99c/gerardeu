5fedc79b3e30fcd22ce9012ec83754f917d258d8
Gery99c <gerard.cervera.costa@gmail.com>
2025-02-18 10:34:14 +0100

    Update file

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    target: 'esnext',
-    minify: 'terser',
-    cssMinify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  plugins: [react()]
 })
