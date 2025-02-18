35ed3e54029cf8f79fb9fde18d0601191637920c
Gery99c <gerard.cervera.costa@gmail.com>
2025-02-18 10:31:40 +0100

    Update file

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false, // Disable sourcemaps in production
+    target: 'esnext',
     minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
+    cssMinify: true,
     rollupOptions: {
       output: {
-        manualChunks: {
-          'vendor': ['react', 'react-dom'],
-          'animations': ['framer-motion'],
-          'icons': ['react-icons']
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+        manualChunks: undefined
       }
     }
-  },
-  server: {
-    port: 3000,
-    strictPort: true
-  },
-  preview: {
-    port: 3000,
-    strictPort: true
   }
 })
