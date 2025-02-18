96f05efb38969eb5f21796c894019d44fc897734
Gery99c <gerard.cervera.costa@gmail.com>
2025-02-18 12:17:29 +0100

    corregimos fallo permisos

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
+    outDir: 'dist'
   }
 })
